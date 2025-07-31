cacaa8ce755958b2ef99d72c0fbd0c42d8f0b678
Kunal Katarya <kunal.rox@gmail.com>
2025-07-31 17:00:50 +0800

    third party package

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -2,7 +2,24 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["qn_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["qn_text"]
+    fieldsets = [
+        (None, {"fields": ["qn_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 
 # Register your models here.
